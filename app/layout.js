26ff426b2416d6c246706b8e20e7e6d6c35675f6
Mateus Saraiva <mateus.saraiva09@outlook.com>
2026-05-02 09:05:09 -0300

    Add sidebar in dashboard layout

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider"
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -20,9 +22,19 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="pt-br"
+      suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
