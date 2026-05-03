ab8f97289f9962da155b5faf08b393714ec41362
Mateus Saraiva <mateus.saraiva09@outlook.com>
2026-05-03 08:33:12 -0300

    Add one column in grid

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
-    <div className="flex flex-1 flex-col gap-4 p-4">
-      <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+    <div className="flex flex-1 flex-col gap-4 p-6">
+      <div className="grid auto-rows-min gap-4 md:grid-cols-4">
         <div className="aspect-video rounded-xl bg-muted/50" />
         <div className="aspect-video rounded-xl bg-muted/50" />
         <div className="aspect-video rounded-xl bg-muted/50" />
